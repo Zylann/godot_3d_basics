b4e95ba3ca4ba5d59529249f5b46a3a785fb1640
Marc Gilleron <marc.gilleron@gmail.com>
2022-10-01 22:06:42 +0100

    Workaround for Godot not allowing to select child nodes of a scene in the nodepath dialog

--- addons/zylann.3d_basics/camera/camera.gd
+++ addons/zylann.3d_basics/camera/camera.gd
@@ -8,6 +8,9 @@ var _head : Node3D
 
 func _ready():
 	_head = get_node(_head_path)
+	# Workaround for Godot not allowing to select child nodes of a scene in the nodepath dialog
+	if _head.has_method("get_head"):
+		_head = _head.get_head()
 
 
 func _process(delta: float):
